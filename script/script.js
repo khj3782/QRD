4747409345b4d81486b294d811c74c3a40d67cbe
김현진 <60802610+khj3782@users.noreply.github.com>
2021-09-21 01:08:25 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,62 @@
+var id1 = null;
+var id2 = null;
+var id3 = null;
+var id4 = null;
+var id5 = null;
+
+function init_position(elem_id){
+  var elem = document.getElementById(elem_id);
+  var x = Math.floor(Math.random() * (window.innerHeight - 140));
+  var y = Math.floor(Math.random() * (window.innerWidth - 140));
+  elem.style.top = x + 'px';
+  elem.style.left = y + 'px';
+}
+
+function move(elem_id, id_num) {
+  var elem = document.getElementById(elem_id);
+  var x = elem.offsetTop;
+  var y = elem.offsetLeft;
+  /*elem.style.top = x + 'px';
+  elem.style.left = y + 'px';*/
+
+  var xStep, yStep;
+  xStep = Math.floor(Math.random() * 10) - 5;
+  yStep = Math.floor(Math.random() * 10) - 5;
+  xStep = xStep * 4 + 1;
+  yStep = yStep * 4 + 1;
+
+  clearInterval(id_num);
+  id_num = setInterval(frame, 30);
+
+  function frame() {
+    x += xStep;
+    y += yStep;
+    if (!boundary(x, y, elem_id)) {
+      clearInterval(id_num);
+      move(elem_id, id_num);
+    } else {
+      elem.style.top = x + 'px';
+      elem.style.left = y + 'px';
+    }
+  }
+}
+
+function boundary(x, y, elem_id) {
+  var elem = document.getElementById(elem_id);
+  if (x < 0) return false;
+  if (y < 0) return false;
+  if (x > (window.innerHeight - elem.offsetHeight)) return false;
+  if (y > (window.innerWidth - elem.offsetWidth)) return false;
+  else return true;
+}
+
+init_position("dot1");
+init_position("dot2");
+init_position("dot3");
+init_position("dot4");
+init_position("dot5");
+move("dot1", id1);
+move("dot2", id2);
+move("dot3", id3);
+move("dot4", id4);
+move("dot5", id5);
